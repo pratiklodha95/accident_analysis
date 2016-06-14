dcd3bb2cd734fd76470398fd6017be6652506d68
pratiklodha95 <lodhapratik9@gmail.com>
2016-06-14 23:22:38 +0530

    forms

--- forms/models.py
+++ forms/models.py
@@ -4,3 +4,16 @@ from django.db import models
 class State(models.Model):
 	state_id = models.CharField(max_length=200)
 	state_name = models.CharField(max_length=200)
+
+class Districts(models.Model):
+	state = models.ForeignKey(State)
+	district_id = models.CharField(max_length=200)
+	district_name = models.CharField(max_length=200)
+
+class AccidentDate(models.Model):
+	district = models.ForeignKey(Districts)
+	firno = models.CharField(max_length=200)
+	policestation = models.CharField(max_length=200)
+	accidentdate = models.DateField()
+
+	# all the other fields in the form have to be included here
